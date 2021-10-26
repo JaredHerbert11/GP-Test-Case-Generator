99f22829553c0799b1ba2308ba6404acb7020dd6
JaredHerbert11 <jaredherberttsa@gmail.com>
2021-10-26 12:23:19 -0400

    Supports any arity

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 import re
 
 class Node:
-    def __init__(self, left, right, value, leaf):
+    def __init__(self, children, value, leaf):
         self.value = value
         self.leaf = leaf
-        self.left = left
-        self.right = right
+        self.children = children
     def post_array(self, nodes=[]):
         if not self.leaf:
-            nodes = [*nodes, *self.left.post_array()]
-            nodes = [*nodes, *self.right.post_array()]
+            for i in range(len(self.children)):
+                nodes = [*nodes, *self.children[i].post_array()]
             nodes.append(self.value)
             return nodes
         else:
@@ -20,13 +19,15 @@ class Node:
 
 def form_tree(nodes, inst):
     value = nodes.pop(0)
-    if value in inst:
-        left = form_tree(nodes, inst)
-        right = form_tree(nodes, inst)
-        newNode = Node(left, right, value, False)
-        return newNode
+    for i in range(len(inst)):
+        if value in inst[i]:
+            children = [None] * (i+1)
+            for j in range(i+1):
+                children[j] = form_tree(nodes, inst)
+            newNode = Node(children, value, False)
+            return newNode
     else:
-        newNode = Node(None, None, value, True)
+        newNode = Node(None, value, True)
         return newNode
 
 file1 = open('input.txt', 'r')
@@ -35,27 +36,29 @@ print('\n')
 file1.close();
  
 count = 0
-inst = ["add", "sub", "mul", "protectedDiv"]
+inst = [["sin", "cos", "tan", "neg"], ["add", "sub", "mul", "protectedDiv"], ["if"]]
 
 file2 = open('output.txt', 'w')
 
 for line in Lines:
     count += 1
-    print("Line{}: {}".format(count, line.strip()))
-    nodes = re.split(r',|\(|\)|\s+', line)
-    nodes = list(filter(None, nodes))
-    print(nodes)
-    tree = form_tree(nodes, inst)
-    
-    reverse_nodes = tree.post_array()
-    print(reverse_nodes)
-    First = True
-    for item in reverse_nodes:
-        if First:
-            First = False
-        else:
-            file2.write(", ")
-        file2.write("%s" % item)
-    file2.write("\n")
-    print("\n")
+    if(line != '\n' and line[0] != '#'):
+        print("Line{}: {}".format(count, line.strip()))
+        nodes = re.split(r',|\(|\)|\s+', line)
+        nodes = list(filter(None, nodes))
+        print(nodes)
+        tree = form_tree(nodes, inst)
+        #print(tree)
+
+        reverse_nodes = tree.post_array()
+        print(reverse_nodes)
+        First = True
+        for item in reverse_nodes:
+            if First:
+                First = False
+            else:
+                file2.write(", ")
+            file2.write("%s" % item)
+        file2.write("\n")
+        print("\n")
 file2.close()
